f6a24e351dd802b871c9ea9613dd37b6e5b4427a
underthebluemoon <underthebluemoon0@gmail.com>
2025-12-22 18:15:21 +0900

    251222 고객 타입, 서비스 타입, 서비스 이름. 상수 추가

---
+++ configs/service.type.enum.js
@@ -0,0 +1,15 @@
+/**
+ * @file configs/user.type.enum.js
+ * @description 유저 타입 상수 설정
+ * 251222 v1.0.0 N init
+ */
+
+export const SERVICE_TYPE = Object.freeze({
+  DELIVERY: 'D',
+  STORAGE: 'S',
+});
+
+export const SERVICE_NAME = Object.freeze({
+  DELIVERY: 'DELIVERY',
+  STORAGE: 'STORAGE',
+});
