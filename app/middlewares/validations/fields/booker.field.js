3315da3debb3a83dc8db536d7427abac70fa8e83
KMinHyun <kimdennis264@gmail.com>
2025-12-31 12:54:46 +0900

    251231 관리자 예약 관리 페이지 관련 파일 수정

--- app/middlewares/validations/fields/booker.field.js
+++ app/middlewares/validations/fields/booker.field.js
@@ -68,6 +68,34 @@ const userName = body('userName')
   .withMessage('비밀번호는 4자에서 20자까지 입력 가능합니다.')
 ;
 
+// ------------------------------------------
+// 예약 관리용
+// ------------------------------------------
+// userId가 없을 때만 필수 체크 (if 조건 사용)
+const bookerName = body('bookerInfo.userName')
+  .if(body('userId').not().exists({ checkNull: true })) // userId가 없거나 null이면 검사
+  .trim()
+  .notEmpty()
+  .withMessage('비회원 예약 시 예약자 이름은 필수입니다.')
+;
+
+const bookerEmail = body('bookerInfo.email')
+  .if(body('userId').not().exists({ checkNull: true }))
+  .trim()
+  .notEmpty()
+  .withMessage('이메일은 필수입니다.')
+  .bail()
+  .isEmail()
+  .withMessage('이메일 형식이 올바르지 않습니다.')
+;
+
+const bookerPhone = body('bookerInfo.phone')
+  .if(body('userId').not().exists({ checkNull: true }))
+  .trim()
+  .notEmpty()
+  .withMessage('연락처는 필수입니다.')
+;
+
 export default {
   phone,
   email,
@@ -76,4 +104,8 @@ export default {
   userName,
   // 조회
   lookupPassword,
+  // 예약 관리
+  bookerName,
+  bookerEmail,
+  bookerPhone,
 };
--- routes/admin/admin.reservations.router.js
+++ routes/admin/admin.reservations.router.js
@@ -7,16 +7,16 @@ import express from 'express';
 import adminReservationsController from '../../app/controllers/admins/admin.reservations.controller.js';
 import authMiddleware from '../../app/middlewares/auth/auth.middleware.js';
 import validationHandler from '../../app/middlewares/validations/validation.handler.js';
-import { storeValidator, updateValidator } from '../../app/middlewares/validations/validators/reserve/admin.reservation.validator.js';
+import { idValidator, indexValidator, storeValidator, updateValidator } from '../../app/middlewares/validations/validators/reserve/admin.reservation.validator.js';
 
 const adminReservationRouter = express.Router();
 
 adminReservationRouter.use(authMiddleware);
 
-adminReservationRouter.get('/',  adminReservationsController.index);
-adminReservationRouter.get('/:id', adminReservationsController.show);
+adminReservationRouter.get('/', indexValidator, validationHandler, adminReservationsController.index);
+adminReservationRouter.get('/:id',idValidator, validationHandler, adminReservationsController.show);
 adminReservationRouter.post('/', storeValidator, validationHandler, adminReservationsController.store);
-adminReservationRouter.patch('/:id', updateValidator, validationHandler, adminReservationsController.update);
-adminReservationRouter.delete('/:id', adminReservationsController.destroy);
+adminReservationRouter.patch('/:id', idValidator, updateValidator, validationHandler, adminReservationsController.update);
+adminReservationRouter.delete('/:id',idValidator, validationHandler, adminReservationsController.destroy);
 
 export default adminReservationRouter;
