57c2e675b2076d84c4190e4beafa416d17cd8ee9
underthebluemoon <underthebluemoon0@gmail.com>
2026-01-04 23:28:34 +0900

    260104 후기 작성 ( 작성 가능한 예약 정보 가져오기, 후기 데이터 추가하기), 후기 삭제 (후기 데이터 삭제 )

--- routes/user/user.review.router.js
+++ routes/user/user.review.router.js
@@ -5,16 +5,24 @@
  */
 
 import express from 'express';
+// ========================
+// ||     middleware     ||
 // ===== validators
 import indexValidator from '../../app/middlewares/validations/validators/review/index.validator.js';
-import showValidator from "../../app/middlewares/validations/validators/review/show.validator.js"
+import createValidator from '../../app/middlewares/validations/validators/review/create.validator.js';
+import destroyValidator from '../../app/middlewares/validations/validators/review/destory.validator.js'
 import validationHandler from '../../app/middlewares/validations/validation.handler.js';
+
+import authMiddleware from '../../app/middlewares/auth/auth.middleware.js';
+import multerMiddleware from '../../app/middlewares/multer/multer.middleware.js';
 // ===== controllers
 import reveiwController from "../../app/controllers/user/review.controller.js"
 
 const userReviewRouter = express.Router();
 
 userReviewRouter.get('/', indexValidator, validationHandler, reveiwController.index);
-userReviewRouter.post('/create', reveiwController.create);
+userReviewRouter.get('/reviewable', authMiddleware, reveiwController.reviewable);
+userReviewRouter.post('/', authMiddleware, multerMiddleware.reviewUploader, createValidator, validationHandler, reveiwController.create);
+userReviewRouter.delete('/:id', authMiddleware, destroyValidator, validationHandler, reveiwController.destroy);
 
 export default userReviewRouter;
