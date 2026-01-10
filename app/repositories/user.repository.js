c36e201b1a14a0f4bbb842b80ccda0e54f5da07a
underthebluemoon <underthebluemoon0@gmail.com>
2026-01-10 13:59:33 +0900

    260110 관리자페이지 유저관리 관련 로직 완료 : 유저 수정(차단,메모), 삭제(탈퇴), 페이지네이션(상태필터링, 이름&이메일 검색), 유효성 검사

--- app/repositories/user.repository.js
+++ app/repositories/user.repository.js
@@ -5,7 +5,7 @@
  */
 
 import db from '../models/index.js';
-const { User } = db;
+const { User, Reservation } = db;
 
 /**
  * 이메일로 유저 검색
@@ -50,10 +50,22 @@ async function findByPk(t = null, id) {
   return await User.findByPk(id, {transaction: t});
 }
 
+/**
+ * 유저 생성
+ * @param {*} t 
+ * @param {*} data 
+ * @returns 
+ */
 async function create(t = null, data) {
   return await User.create(data, { transaction: t });
 }
 
+/**
+ * 유저 로그아웃
+ * @param {*} t 
+ * @param {*} id 
+ * @returns 
+ */
 async function logout(t = null, id) {
   return await User.update(
     {
@@ -68,10 +80,119 @@ async function logout(t = null, id) {
   );
 }
 
+// ===== 관리자 페이지
+
+/**
+ * 유저 페이지네이션
+ * @param {*} t 
+ * @param {*} data 
+ * @returns 
+ */
+async function pagination(t = null, { where, offset, limit }) {
+  return await User.findAndCountAll({
+      order: [
+        ['createdAt', 'DESC'],
+        ['updatedAt', 'DESC'],
+      ],
+      limit,
+      offset,
+      where,
+      transaction: t,
+    });
+}
+
+/**
+ * 유저 id로 조회 + 예약 통계 가져오기
+ * @param {import("sequelize").Transaction} t 
+ * @param {number} id 
+ * @returns {Promise<ImportAttributes("../models/User.js").User>}
+ */
+async function findByPkJoinReservation(t = null, { id }) {
+  return await User.findByPk(
+    id, 
+    {
+      // 통계 데이터
+      attributes: {
+        include: [
+          [
+            db.sequelize.literal(`(
+              SELECT COALESCE(SUM(price), 0)
+                FROM reservations AS r
+                WHERE r.user_id = User.id
+                AND r.state = 'COMPLETED'
+                AND r.deleted_at IS NULL
+            )`),
+            'totalPayment'
+          ],
+          [
+            db.sequelize.literal(`(
+              SELECT COUNT(*)
+              FROM reservations AS r
+              WHERE r.user_id = User.id
+              AND r.deleted_at IS NULL
+            )`),
+            'reservationCount'
+          ]
+        ]
+      },
+      include: [
+        {
+          model: Reservation,
+          as: 'userIdReservations',
+          limit: 5,
+          order: [
+            ['createdAt', 'DESC']
+          ]
+        }
+      ],
+      transaction: t
+    }
+  );
+}
+
+/**
+ * 유저 수정 - statu, adminMemo
+ * @param {*} t 
+ * @param {*} param1 
+ * @returns 
+ */
+async function update(t = null, { id, status, adminMemo }) {
+  return await User.update(
+    {
+      status,
+      adminMemo, 
+    },
+    {
+      where: { id },
+      transaction: t
+    }
+  )
+}
+
+/**
+ * 유저 삭제
+ * @param {*} t 
+ * @param {*} param1 
+ * @returns 
+ */
+async function destroy(t = null, { id }) {
+  return await User.destroy(
+    {
+      where: { id },
+      transaction: t
+    }
+  );
+}
+
 export default {
   findByEmail,
   save,
   findByPk,
   create,
   logout,
+
+  pagination,
+  findByPkJoinReservation,
+  update,
+  destroy,
 }
